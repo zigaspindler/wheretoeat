e3f42168b631ae2811f6baba6ec34e06bb0cd63b
Ziga Spindler <ziga.spindler@gmail.com>
2015-11-02 15:53:36 +0100

    show non-regular first

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,13 +5,13 @@ class Restaurant < ActiveRecord::Base
   def grouped_menus
     menus_array = []
     for date in Date.today..4.days.from_now
-      menus_array << {date: date, menus: menus.where('date=? OR regular=true', date)}
+      menus_array << {date: date, menus: menus.where('date=? OR regular=true', date).order(:regular)}
     end
     menus_array
   end
 
   def todays_menus
-    @todays_menus ||= menus.where('date=? OR regular=true', Date.today)
+    @todays_menus ||= menus.where('date=? OR regular=true', Date.today).order(:regular)
   end
 
   def today_votes_number
