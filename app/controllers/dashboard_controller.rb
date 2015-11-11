4e9cb8f7a41d7fb4d257a5aa5dedf6a3aaf3e56a
Ziga Spindler <ziga.spindler@gmail.com>
2015-11-11 09:59:05 +0100

    random restaurant list per day

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,15 @@
 class DashboardController < ApplicationController
   def index
-    @restaurants = Restaurant.order :name
+    Restaurant.connection.execute "select setseed(#{seed})"
+    @restaurants = Restaurant.order 'random()'
     @comments = Comment.where("created_at >= ?", Time.zone.now.beginning_of_day)
     @comment = Comment.new
+  end
+
+  private
+
+  def seed
+    today = Date.today + 1
+    Random.new(today.day * today.month * today.year).rand - 1
   end
 end
