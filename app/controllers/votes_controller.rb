6421cd33fb26c34503cf2441b3d5573665c5feec
Matjaz Lipus <matjazl@gmail.com>
2016-12-20 20:22:09 +0100

    votes overview

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,19 @@
 class VotesController < ApplicationController
+
+  def show
+    date = Date.today
+    @top_restaurant_id = ActiveRecord::Base.connection.exec_query("
+      SELECT restaurants.id
+      FROM restaurants
+      LEFT JOIN votes ON votes.restaurant_id = restaurants.id
+      WHERE votes.date = '#{date}'
+      AND votes.group_id = #{current_group.id}
+      GROUP BY restaurants.id
+      ORDER BY COUNT(votes) DESC
+      LIMIT 1").first()['id']
+    @votes = Vote.where(date: date).includes(:restaurant, :user, :menu).order('restaurants.name ASC, users.username ASC')
+  end
+
   def create
     vote = Vote.find_or_create_by user: current_user, date: Date.today
     if params[:menu_id].nil?
