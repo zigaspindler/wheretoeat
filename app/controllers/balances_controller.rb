4ce3993a0db658e8e8dbd10707cd35af84aa86c5
Matjaz Lipus <matjazl@gmail.com>
2020-08-14 14:40:28 +0200

    added splitwise

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -6,18 +6,37 @@ class BalancesController < ApplicationController
   private
 
   def balances
-    app_id = current_group.sr_id
-    return [] if app_id.nil? || app_id == ''
+    return fetch_splitwise if current_group.has_splitwise?
+    return fetch_sr if current_group.has_sr?
+    return []
+  end
+
+  def fetch_sr
     begin
+      app_id = current_group.sr_id
       response = HTTParty.get("http://www.shortreckonings.com/ajax.php?tid=#{app_id}&token=54&caller=model&action=payments&")
       JSON.parse(response.body)['data']['totals'].map { |balance|
         user = {
-          balance: balance['own'].to_i - balance['owes'].to_i
+          balance: (balance['own'].to_i - balance['owes'].to_i) / 100.0
         }
 
         user[:name] = User.select(:username).find_by_shortreckonings_id(balance['pid']).try(:username) || balance['pid']
         user
       }[0...-1].sort_by { |u| u[:balance] }
+    rescue
+      []
+    end
+  end
+
+  def fetch_splitwise
+    begin
+      response = HTTParty.get(current_group.splitwise_url)
+      JSON.parse(response.body)['balances'].map do |balance|
+        {
+          name: balance['name'],
+          balance: balance['amount']
+        }
+      end
     rescue
       []
     end
--- app/models/group.rb
+++ app/models/group.rb
@@ -8,7 +8,25 @@ class Group < ActiveRecord::Base
   accepts_nested_attributes_for :users
 
   def balance_table?
+    has_sr? || has_splitwise?
+  end
+
+  def has_sr?
     !(sr_id.nil? || sr_id == '')
+  end
+
+  def has_splitwise?
+    !(splitwise_url.nil? || splitwise_url == '')
+  end
+
+  def has_splitwise_group_id?
+    !(splitwise_group_id.nil? || splitwise_group_id == '')
+  end
+
+  def balance_table_link
+    return "https://www.shortreckonings.com/sr##{sr_id}" if has_sr?
+    return "https://secure.splitwise.com/#/groups/#{splitwise_group_id}" if has_splitwise_group_id?
+    ''
   end
 
   rails_admin do
@@ -25,6 +43,8 @@ class Group < ActiveRecord::Base
       field :sr_id do
         label 'Shortreckonings ID'
       end
+      field :splitwise_url
+      field :splitwise_group_id
       field :users
     end
   end
